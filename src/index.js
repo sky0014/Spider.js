8f99ec456cdb40e2eca8f0a98041d34a78ba806f
sky0014 <sky-wang@qq.com>
2017-06-16 03:32:17 +0800

    handle load fail

--- src/index.js
+++ src/index.js
@@ -5,24 +5,28 @@ import { sleep } from "./util";
 
 const print = console.log;
 
-export function spider(list, callback, options) {
-  const next = async () => {
-    if (list.length > 0) {
-      let url = list.shift();
-      print(`load ${url}`);
-      let result = await rp({
-        uri: url,
-        ...options
-      });
-      let newurl = callback(result);
-      if (newurl) list.push(newurl);
-      // await sleep(5000);
-      next();
-    } else {
-      print("all done!");
-      process.exit();
-    }
-  };
+export default function spider(list, callback, options) {
+    const next = async() => {
+        if (list.length > 0) {
+            let url = list.shift();
+            print(`load ${url}`);
+            try {
+                let result = await rp({
+                    uri: url,
+                    ...options
+                });
+                let newurl = callback(result);
+                if (newurl) list.push(newurl);
+            } catch (e) {
+                print(`load ${url} failed ${e}`);
+            }
+            await sleep(1000);
+            next();
+        } else {
+            print("all done!");
+            process.exit();
+        }
+    };
 
-  next();
+    next();
 }
