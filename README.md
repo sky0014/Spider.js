2807450bbf8ed22d3d27836519293051b48b97a3
sky0014 <sky-wang@qq.com>
2017-06-16 16:45:12 +0800

    v1.0.0 ready to publish

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# Spider.js
+# spider.js
 A simple node spider
+
+## Install
+```bash
+npm install a-spider --save
+```
+
+## Usage
+
+* Definition
+```js
+/**
+ * 
+ * @param {Array|string} list page urls
+ * @param {function} callback handle callback, (html,url)=>next url
+ * @param {object} options 
+ * {
+    spiderDelay = 0, //get delay (ms)
+    spiderBrowserSim = true, //simulate a browser
+    spiderBrowserSimType = "pc", //simulate browser type, available now : pc, android, iphone, ipad
+    spiderComplete, //complete callback
+    ...rpOptions //other request options, see https://www.npmjs.com/package/request-promise
+  }
+ */
+export default function spider(list, callback, options) 
+```
+
+* Example
+```js
+var spider = require("a-spider");
+var cheerio = require("cheerio");
+//test
+spider(
+  "http://www.gamersky.com/ent/201706/916302.shtml",
+  html => {
+    const $ = cheerio.load(html);
+    const next = $(".page_css b").next().attr("href");
+    return next;
+  },
+  {
+    spiderDelay: 1000
+  }
+);
+```
--- src/index.js
+++ src/index.js
@@ -1,32 +1,89 @@
 import rp from "request-promise-native";
-import cheerio from "cheerio";
+import createDebug from "debug";
 
 import { sleep } from "./util";
 
-const print = console.log;
+const debug = createDebug("spider.js");
 
+const USER_AGENTS = {
+  pc:
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36",
+  android:
+    "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Mobile Safari/537.36",
+  iphone:
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1",
+  ipad:
+    "Mozilla/5.0 (iPad; CPU OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1"
+};
+
+function getHomeUrl(url) {
+  if (url) {
+    let a = url.match(/^https?:\/\/[^\/]*/);
+    if (a) return a[0];
+  }
+}
+
+export { sleep };
+
+/**
+ * 
+ * @param {Array|string} list page urls
+ * @param {function} callback handle callback, (html,url)=>next url
+ * @param {object} options 
+ * {
+    spiderDelay = 0, //get delay (ms)
+    spiderBrowserSim = true, //simulate a browser
+    spiderBrowserSimType = "pc", //simulate browser type, available now : pc, android, iphone, ipad
+    spiderComplete, //complete callback
+    ...rpOptions //other request options, see https://www.npmjs.com/package/request-promise
+  }
+ */
 export default function spider(list, callback, options) {
-    const next = async() => {
-        if (list.length > 0) {
-            let url = list.shift();
-            print(`load ${url}`);
-            try {
-                let result = await rp({
-                    uri: url,
-                    ...options
-                });
-                let newurl = callback(result);
-                if (newurl) list.push(newurl);
-            } catch (e) {
-                print(`load ${url} failed ${e}`);
-            }
-            await sleep(1000);
-            next();
-        } else {
-            print("all done!");
-            process.exit();
-        }
-    };
+  if (typeof list === "string") list = [list];
+  let {
+    spiderDelay = 0, //get delay (ms)
+    spiderBrowserSim = true, //simulate a browser
+    spiderBrowserSimType = "pc", //simulate browser type, available now : pc, android, iphone, ipad
+    spiderComplete, //complete callback
+    ...rpOptions //other request options, see https://www.npmjs.com/package/request-promise
+  } = options;
+  if (!USER_AGENTS[spiderBrowserSimType]) {
+    debug(
+      `spiderBrowserSimType must be one of pc, android, iphone, ipad, wrong type ${spiderBrowserSimType}, change back to "pc"`
+    );
+    spiderBrowserSimType = "pc";
+  }
 
-    next();
+  const next = async () => {
+    if (list.length > 0) {
+      let url = list.shift();
+      debug(`load ${url}`);
+      //添加headers模拟浏览器，防止被屏蔽
+      let _rpOptions = { ...rpOptions };
+      if (spiderBrowserSim) {
+        const headers = {
+          "User-Agent": USER_AGENTS[spiderBrowserSimType],
+          Referer: getHomeUrl(url)
+        };
+        _rpOptions.headers = { ...headers, ...rpOptions.headers };
+      }
+      try {
+        let result = await rp({
+          uri: url,
+          ..._rpOptions
+        });
+        let newurl = callback(result, url);
+        if (newurl) list.push(newurl);
+      } catch (e) {
+        debug(`load ${url} failed ${e}`);
+      }
+      if (spiderDelay > 0) await sleep(spiderDelay);
+      next();
+    } else {
+      debug("all done!");
+      if (spiderComplete) spiderComplete();
+    }
+  };
+
+  next();
 }
